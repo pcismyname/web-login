ddbd329943fb7b83fed2b633ac995629e5c7c88e
pcismyname <51116872+pcismyname@users.noreply.github.com>
2020-04-27 21:58:33 +0700

    No.2

--- app3.py
+++ app3.py
@@ -1,69 +1,83 @@
-# เขียนโค้ดพยายาม comment ด้วยนะจะได้รู้ว่าแต่ละ def ทำอะไร?
-# ทำให้สามารถแบ่งโค้ดเป็นส่วนๆได้ด้วย จะง่ายต่อการพัฒนานะครับ
-# พี่จะยกตัวอย่างส่วนแรกให้นะ ตัวอย่างคือ comment ที่เป็นภาษาอังกฤษล้วนนะ
-# ถ้าสังเกตุจะเห็นว่าพี่จัดหมดหมู่เอานะครับ
-
-# import library for web app
+#import library for web app
 from flask import *
 
-# import library python connect to mongoDB
+#import library  for password
+import bcrypt
+
+#import library for mongo database
 import pymongo
 from pymongo import MongoClient
 
-# running and setting about web app
-app = Flask(__name__,template_folder='html01')
-# คำอธิบายเพิ่มเติมคือ ส่วนของ template_folder ค่า default คือ template_folder='templates'
-# ไม่ต้องตั้งค่าก็ได้ อันนี้บอกไว้เฉยๆ
-
-# connect to mongoDB
+#connect to MongoDB
 client = MongoClient()
 myclient =pymongo.MongoClient("mongodb://localhost:27017/")
-db = client.login
-col = db.data
-
+app = Flask(__name__,template_folder='html01')
+db = client.login #db for acces datanase name login
+col = db.data    #col for access column name data
 print(myclient.list_database_names())
 
+
+#first page
 @app.route('/')
 def host():
+    if 'username' in session :
+        return 'You are logged in as ' + session['username']
+
     return render_template('index.html')
 
 
-# ทำไมเป็น  / เฉยๆครับ พี่อยากได้ /register นาาาาา
-@app.route('/',methods=['POST'])
+@app.route('/register',methods=['POST','GET'])
 def submit():
-    name = request.form['name']
-    surename = request.form['surename']
-    email = request.form['email']
-    contact = request.form['contact']
-    gender = request.form['gender']
-    user = request.form['user']
-    password = request.form['pass']
-    col.insert_one({'name': name,'surenane':surename,'email':email,'contact':contact,
-                    'gender':gender,'password':password,'username':user})
-    return render_template('login.html')
 
-# ถ้าไม่ต้อง render_template ใหม่ทุกครั้งสามารถทำได้ไหม 
-# แบบเป็นการ redirection แทนอะไรงี้ ได้ไม่ได้ก็บอกนะ
-@app.route('/back',methods=['POST'])
-def back():
+    #store data from form in variable
+    if request.method == 'POST' :
+        name = request.form['name']
+        surename = request.form['surename']
+        email = request.form['email']
+        contact = request.form['contact']
+        gender = request.form['gender']
+        user = request.form['user']
+        password = request.form['pass']
+        existing_user = col.find_one({'username' : user})
+
+        #check if user already exist
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()) #hash the password
+
+            #insert data in database
+            col.insert_one({'name' : name, 'surenane' : surename, 'email' : email, 'contact' : contact,
+                    'gender' : gender, 'password' : hashpass, 'username':user})
+
+            session['username'] = name
+            return redirect(url_for('host'))
+
+        return 'That username already exist'
     return render_template('index.html')
 
-@app.route('/login/',methods=['POST'])
+
+
+#login page
+@app.route('/login/',methods=['POST','GET'])
 def login():
+    if request.method == 'POST' :
+            login_user = col.find_one({'username' : request.form['user']})
+            if login_user:
+
+                #check the password
+                if bcrypt.hashpw(request.form['pass'].encode('utf-8'),  login_user['password']) ==  login_user['password']:
+                    session['username'] = login_user['name']
+                    return redirect(url_for('host'))
+
+            return 'Invalid username or password'
     return render_template('login.html')
 
-@app.route('/back2',methods=['POST'])
-def back2():
-    return render_template('login.html')
 
+#display page
 @app.route('/login/display',methods=['POST'])
 def display():
     return render_template('display.html')
 
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True,port=50)
-    # พี่อยากได้ port 80 ครับ
-    
+    app.secret_key = 'mysecret'
+    app.run(debug=True,port=80)
